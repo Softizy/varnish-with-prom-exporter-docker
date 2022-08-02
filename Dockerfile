ac0a09eb241cdd72f89d030a38f1d82cd5b3de66
Mickael <mickael@softizy.com>
2022-08-02 16:25:52 +0200

    add signaller from mittwald/kube-httpcache

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM varnish:7.1
+FROM quay.io/mittwald/kube-httpcache:stable as kube-httpcache
+FROM varnish:7.1 as final
 
 LABEL org.opencontainers.image.authors="tech@softizy.com"
 LABEL org.opencontainers.image.source="https://github.com/Softizy/varnish-with-prom-exporter-docker"
@@ -8,6 +9,7 @@ USER root
 ENV VARNISH_EXPORTER_VERSION=1.6.1
 
 ADD https://github.com/jonnenauha/prometheus_varnish_exporter/releases/download/${VARNISH_EXPORTER_VERSION}/prometheus_varnish_exporter-${VARNISH_EXPORTER_VERSION}.linux-amd64.tar.gz /tmp
+COPY --from=kube-httpcache /kube-httpcache /kube-httpcache
 COPY startup-script.sh /startup-script.sh
 
 RUN set -eux; \
@@ -15,7 +17,18 @@ RUN set -eux; \
     mv /tmp/prometheus_varnish_exporter-${VARNISH_EXPORTER_VERSION}.linux-amd64/prometheus_varnish_exporter /usr/sbin/; \
     chmod +x /startup-script.sh;
 
-ENTRYPOINT ["/startup-script.sh"]
-
 USER varnish
-EXPOSE 8080 8443 9131
+
+ENV ADMIN_PORT=6083
+ENV SIGNALLER_PORT=8090
+ENV FRONTEND_NAMESPACE=""
+ENV FRONTEND_SERVICE=""
+ENV FRONTEND_PORT=8080
+ENV BACKEND_NAMESPACE=""
+ENV BACKEND_SERVICE=""
+ENV SECRET_PATH=/etc/varnish/k8s-secret/secret
+ENV VCL_TEMPLATE=/etc/varnish/tmpl/default.vcl.tmpl
+ENV VARNISH_STORAGE="malloc,128M"
+
+ENTRYPOINT []
+CMD ["/startup-script.sh"]
--- startup-script.sh
+++ startup-script.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 set -e
 
-# this will check if the first argument is a flag
-# but only works if all arguments require a hyphenated flag
-# -v; -SL; -f arg; etc will work, but not arg1 arg2
-if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
-    set -- varnishd \
-	    -F \
-	    -f /etc/varnish/default.vcl \
-	    -a http=:8080,HTTP \
-	    -a proxy=:8443,PROXY \
-	    -p feature=+http2 \
-	    -s malloc,$VARNISH_SIZE \
-	    -n /tmp/varnish \
-	    "$@"
-fi
+trap "exit 1" TERM
+export TOP_PID=$$
 
-exec "$@" &
+function missingEnv() {
+    echo "Missing env var $1"
+    kill -s TERM $TOP_PID
+}
 
-/usr/sbin/prometheus_varnish_exporter -web.listen-address "0.0.0.0:9131" -web.telemetry-path "/metrics" -web.health-path "/health" &
+# Varnish / Signaller vars check
+[[ -z "${ADMIN_PORT}" ]] && missingEnv ADMIN_PORT || AdminPort="${ADMIN_PORT}"
+[[ -z "${SIGNALLER_PORT}" ]] && missingEnv SIGNALLER_PORT || SignallerPort="${SIGNALLER_PORT}"
+[[ -z "${FRONTEND_NAMESPACE}" ]] && missingEnv FRONTEND_NAMESPACE || FrontendNamespace="${FRONTEND_NAMESPACE}"
+[[ -z "${FRONTEND_SERVICE}" ]] && missingEnv FRONTEND_SERVICE || FrontendService="${FRONTEND_SERVICE}"
+[[ -z "${FRONTEND_PORT}" ]] && missingEnv FRONTEND_PORT || FrontendPort="${FRONTEND_PORT}"
+[[ -z "${BACKEND_NAMESPACE}" ]] && missingEnv BACKEND_NAMESPACE || BackendNamespace="${BACKEND_NAMESPACE}"
+[[ -z "${BACKEND_SERVICE}" ]] && missingEnv BACKEND_SERVICE || BackendService="${BACKEND_SERVICE}"
+[[ -z "${SECRET_PATH}" ]] && missingEnv SECRET_PATH || SecretPath="${SECRET_PATH}"
+[[ -z "${VCL_TEMPLATE}" ]] && missingEnv VCL_TEMPLATE || VclTemplate="${VCL_TEMPLATE}"
+[[ -z "${VARNISH_STORAGE}" ]] && missingEnv VARNISH_STORAGE || VarnishStorage="${VARNISH_STORAGE}"
+
+# Prometheus exporter vars check
+[[ -z "${PROMETHEUS_EXPORTER_PORT}" ]] && missingEnv PROMETHEUS_EXPORTER_PORT || PrometheusExporterPort="${PROMETHEUS_EXPORTER_PORT}"
+
+/kube-httpcache \
+  -admin-addr=0.0.0.0 \
+  -admin-port="$AdminPort" \
+  -signaller-enable \
+  -signaller-port="$SignallerPort" \
+  -frontend-watch \
+  -frontend-namespace="$FrontendNamespace" \
+  -frontend-service="$FrontendService" \
+  -frontend-port="$FrontendPort" \
+  -backend-watch \
+  -backend-namespace="$BackendNamespace" \
+  -backend-service="$BackendService" \
+  -varnish-secret-file="$SecretPath" \
+  -varnish-vcl-template="$VclTemplate" \
+  -varnish-storage="$VarnishStorage" \
+  &
+
+/usr/sbin/prometheus_varnish_exporter -web.listen-address "0.0.0.0:$PrometheusExporterPort" -web.telemetry-path "/metrics" -web.health-path "/health" &
 
 wait -n
 
